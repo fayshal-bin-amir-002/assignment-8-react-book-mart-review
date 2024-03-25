1c0c96ce99503150a832687991e4db73bfac1c61
fayshal69 <foyshalbinamir@gmail.com>
2024-03-26 00:44:57 +0600

    added wishlist books

--- src/components/ReadBooks.jsx
+++ src/components/ReadBooks.jsx
@@ -1,14 +1,15 @@
+import { getData } from "../utilities/localStorage";
+import SingleReadBooks from "./SingleReadBooks";
+
 const ReadBooks = () => {
+
+    const books = getData();
+
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">New movie is released!</h2>
-                <p>Click the button to watch on Jetflix app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
-                </div>
-            </div>
+        <div className="mb-12 lg:mb-24">
+            {
+                books.map((book) => <SingleReadBooks key={book.bookId} book={book}></SingleReadBooks>)
+            }
         </div>
     );
 };
--- src/components/WishlistBooks.jsx
+++ src/components/WishlistBooks.jsx
@@ -1,14 +1,15 @@
+import { getWishlistBook } from "../utilities/localStorage";
+import SingleWishlistBook from "./SingleWishlistBook";
+
 const WishlistBooks = () => {
+
+    const books = getWishlistBook();
+
     return (
-        <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Fayshal</h2>
-                <p>Click the button to watch on Jetflix app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Watch</button>
-                </div>
-            </div>
+        <div className="mb-12 lg:mb-24">
+            {
+                books.map((book) => <SingleWishlistBook key={book.bookId} book={book}></SingleWishlistBook>)
+            }
         </div>
     );
 };
