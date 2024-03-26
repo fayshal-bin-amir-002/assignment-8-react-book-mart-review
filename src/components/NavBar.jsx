4b7dba3a85dd1fd6bf9203a170fd757c2b3e8b6a
fayshal69 <foyshalbinamir@gmail.com>
2024-03-26 20:52:21 +0600

    updated navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
     return (
         <div className="container mx-auto">
-            <div className="navbar justify-between items-center bg-base-100 py-6 lg:py-12 fixed top-0 z-10 bg-opacity-90 container mx-auto">
-                <div className=" w-auto">
+            <div className="navbar justify-between items-center bg-base-100 py-3 lg:py-6 fixed top-0 z-10 bg-opacity-90 container mx-auto">
+                <div className="flex justify-between w-full lg:w-auto">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 space-y-3 z-[1] p-2 shadow bg-base-100 rounded-box w-36">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 space-y-3 z-10 p-2 shadow bg-base-100 rounded-box w-36">
                             <li><NavLink to="/" className={({ isActive }) => isActive ? 'bg-transparent text-[#23BE0A] border border-[#23BE0A]' : ''}>Home</NavLink></li>
                             <li><NavLink to="/lited-books" className={({ isActive }) => isActive ? 'bg-transparent text-[#23BE0A] border border-[#23BE0A]' : ''}>Listed Books</NavLink></li>
                             <li><NavLink to="/pages-to-read" className={({ isActive }) => isActive ? 'bg-transparent text-[#23BE0A] border border-[#23BE0A]' : ''}>Pages to Read</NavLink></li>
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -4,7 +4,7 @@ import NavBar from "../components/NavBar";
 const MainLayout = () => {
     return (
         <>
-            <header className="h-24 lg:h-[158px] mb-6 lg:mb-12">
+            <header className="h-20 lg:h-[110px] mb-4 lg:mb-8">
                 <NavBar></NavBar>
             </header>
             <main className="container mx-auto px-5">
